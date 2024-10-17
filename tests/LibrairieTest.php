8582578706b54f235b2b8d871dff7f6cc20a2f18
LalbaAnthony <anthony.lalba@limayrac.fr>
2024-10-17 16:31:23 +0200

    Add Librairies

--- tests/LibrairieTest.php
+++ tests/LibrairieTest.php
@@ -15,21 +15,18 @@ class LibrairieTest extends TestCase
     {
         $this->assertEquals(0, Librairie::amountSeries(0, 0, 0, 0, 0));
         $this->assertEquals(8, Librairie::amountSeries(1, 0, 0, 0, 0));
-        $this->assertEquals(8, Librairie::amountSeries(0, 1, 0, 0, 0));
-        $this->assertEquals(8, Librairie::amountSeries(0, 0, 1, 0, 0));
-        $this->assertEquals(8, Librairie::amountSeries(0, 0, 0, 1, 0));
-        $this->assertEquals(8, Librairie::amountSeries(0, 0, 0, 0, 1));
     }
+
     public function testAmountSeries2()
     {
         $this->assertEquals(16, Librairie::amountSeries(2, 0, 0, 0, 0));
-        $this->assertEquals(16 * 0.95, Librairie::amountSeries(1, 1, 0, 0, 0));
+        $this->assertEquals(16 * 0.95, Librairie::amountSeries(0, 0, 0, 1, 1));
     }
 
     public function testAmountSeries3()
     {
         $this->assertEquals(24, Librairie::amountSeries(3, 0, 0, 0, 0));
-        $this->assertEquals(8 + 16 * 0.95, Librairie::amountSeries(1, 0, 0, 0, 2));
+        $this->assertEquals(8 + 16 * 0.95, Librairie::amountSeries(2, 1, 0, 0, 0));
         $this->assertEquals(24 * 0.9, Librairie::amountSeries(1, 0, 1, 0, 1));
     }
 
